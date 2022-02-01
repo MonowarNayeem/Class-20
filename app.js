e17e088039daca4d8be37201a72ef936df137f75
monowar-lwd <a1monoar@gmail.com>
2022-02-01 21:32:28 +0600

    class20

---
+++ app.js
@@ -0,0 +1,10 @@
+//   Toast JS Start
+window.onload = (event) => {
+    let myAlert = document.querySelector('.toast');
+    let bsAlert = new bootstrap.Toast(myAlert);
+    bsAlert.show();
+}
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+        var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+        })
